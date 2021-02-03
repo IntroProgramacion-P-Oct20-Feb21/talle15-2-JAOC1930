9ae9654c891a4fe76d0695371ba6e398f4bc9bf9
Jhoel Ordoñez <jhoelordonez2@gmail.com>
2021-02-02 21:42:33 -0500

    Deber

--- Taller152/src/paqueteuno/EscribirArchivo.java
+++ Taller152/src/paqueteuno/EscribirArchivo.java
@@ -5,12 +5,36 @@
  */
 package paqueteuno;
 
-import paquetetres.*;
+import java.util.Formatter;
 
 /**
  *
  * @author reroes
  */
 public class EscribirArchivo {
-    
+    public static void hola() {
+        Formatter archivo = null;
+        try {
+            String ruta = "/data/archivo1.txt";
+            String[] paises = {"Ecuador", "Colombia", "Perú", "Venezuela", 
+                "Chile", "Bolivia"};
+            String[] capitales = {"Quito", "Bogotá", "Lima", "Caracas", 
+                "Santiago", "La Paz"};
+            int[] poblacion = {17643054, 51152371, 33050325, 28435940, 19116201,
+                11673021};
+            String cadena = "";
+            archivo = new Formatter("data/archivo1.txt");
+            for (int i = 0; i < paises.length; i++){
+                archivo.format("%s %s, con capital %s tiene una poblacion"
+                        + " de %d\n", cadena, paises[i], 
+                        capitales[i], poblacion[i]);
+            }
+        }catch (Exception e) {
+            System.err.printf("Error, revise: %s\n", e.toString());
+
+        }finally{
+            archivo.close();
+        }
+       
+    }
 }
